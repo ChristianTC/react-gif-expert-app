c4bcf634335537a88653f38e1be53ab7d607d0aa
christiantc <christiantambo15@gmail.com>
2022-05-16 10:32:06 -0400

    show the images titles

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,9 +1,10 @@
 import React, {useState, useEffect} from 'react'
+import { GifGridItem } from './GifGridItem'
 
 export const GifGrid = ({ category }) => {
-  
-  const [count, setCount] = useState(0)
-  
+
+  const [images, setImages] = useState([])
+
   useEffect(() => {
     getGifs()
   }, [])
@@ -23,15 +24,23 @@ export const GifGrid = ({ category }) => {
     })
 
     console.log(gifs);
-
+    setImages(gifs)
   }
 
 
   return (
     <div>
       <h3>{category}</h3>
-      <h3>{count}</h3>
-      <button onClick={ () => setCount( count + 1)}></button>
+        {
+          images.map((image) => (
+            <GifGridItem 
+              key={image.id} 
+              {...image} 
+            />
+          ))
+        }
+
+
     </div>
   )
 }
